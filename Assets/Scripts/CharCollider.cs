36a33af0ca7f441265eb28ac63667d82d1118d08
Jeremy Gagnier <jer.gagnier@gmail.com>
2017-09-17 19:13:08 -0400

    unimplemented collider

---
+++ Assets/Scripts/CharCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharCollider : MonoBehaviour {
+    public FVector position;
+    public FInt width;
+    public FInt height;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 1f, 0f, 0.5f);
+        Gizmos.DrawCube(
+            new Vector3(position.x.ToFloat(), position.y.ToFloat()),
+            new Vector3(width.ToFloat(), height.ToFloat()));
+    }
+}
